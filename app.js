c2b56f46a5738f050d820f5eb1b3c6a7457b1a0b
Mikhail <mihail.kuznetsov.99@mail.ru>
2021-04-21 10:40:48 +0300

    done select currency

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
 
 let btn2 = document.querySelector('.in-section');
-let pInpuntFrom = document.querySelector('.from-currency-value')
-let pInpuntTo = document.querySelector('.to-currency-value')
+let pInpuntFrom = document.querySelector('.from-currency-value');
+let pInpuntTo = document.querySelector('.to-currency-value');
+let inputFromValue = document.querySelector('.ihave-input');
 
+
+
+inputFromValue.value = '1';
 let from = 'RUB';
-let to = 'CZK';
+let to = 'USD';
 
 
-async function returnValueFromAPI(el) {
+async function returnValueFromAPI() {
 
-    let response2 = await fetch(`https://api.ratesapi.io/api/latest?symbols=${el}&base=${to}`);
+    let response2 = await fetch(`https://api.ratesapi.io/api/latest?symbols=${from}&base=${to}`);
     let data2 = await response2.json();
 
-    let response = await fetch(`https://api.ratesapi.io/api/latest?symbols=${to}&base=${el}`);
+    let response = await fetch(`https://api.ratesapi.io/api/latest?symbols=${to}&base=${from}`);
     let data = await response.json();
-    return { data: data.rates[to], data2: data2.rates[el] }
+    return { data: data.rates[to], data2: data2.rates[from] }
 }
 
-returnValueFromAPI(from)
+returnValueFromAPI()
     .then((data) => {
         console.log(data);
         pInpuntFrom.innerText = ` 1 ${from} = ${data.data.toFixed(4)} ${to}`;
@@ -26,16 +30,68 @@ returnValueFromAPI(from)
     })
 
 
-function returnButtonValue() {
+function returnFromButtonValue() {
+
+    let selectFrom = document.querySelector('.drop-from')
     let btn = document.querySelectorAll('.ih-section');
+
+        selectFrom.addEventListener('change', (event) => {
+            from = event.target.value
+            console.log(from)
+            returnValueFromAPI()
+            .then((data) => {
+                console.log(data);
+                pInpuntFrom.innerText = ` 1 ${from} = ${data.data.toFixed(4)} ${to}`;
+                pInpuntTo.innerText = ` 1 ${to} = ${data.data2.toFixed(4)} ${from}`;
+            })
+        });
+
     btn.forEach((item) => {
         item.addEventListener('click', () => {
             from = item.innerText
             console.log(from)
+            returnValueFromAPI()
+            .then((data) => {
+                console.log(data);
+                pInpuntFrom.innerText = ` 1 ${from} = ${data.data.toFixed(4)} ${to}`;
+                pInpuntTo.innerText = ` 1 ${to} = ${data.data2.toFixed(4)} ${from}`;
+            })
         });
     })
-    return from
+ 
+}; 
+
+function returnToButtonValue() {
+
+    let btn = document.querySelectorAll('.in-section');
+    let selectFrom = document.querySelector('.drop-to')
+
+    selectFrom.addEventListener('change', (event) => {
+        to = event.target.value
+        console.log(to)
+        returnValueFromAPI()
+        .then((data) => {
+            console.log(data);
+            pInpuntFrom.innerText = ` 1 ${from} = ${data.data.toFixed(4)} ${to}`;
+            pInpuntTo.innerText = ` 1 ${to} = ${data.data2.toFixed(4)} ${from}`;
+        })
+    });
+
+    btn.forEach((item) => {
+        item.addEventListener('click', () => {
+            to = item.innerText
+            console.log(to)
+            returnValueFromAPI()
+            .then((data) => {
+                console.log(data);
+                pInpuntFrom.innerText = ` 1 ${from} = ${data.data.toFixed(4)} ${to}`;
+                pInpuntTo.innerText = ` 1 ${to} = ${data.data2.toFixed(4)} ${from}`;
+        
+            })
+        });
+    })
 };
 
+returnToButtonValue()
+returnFromButtonValue()
 
-returnButtonValue()
